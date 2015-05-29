b9b0d8f6fa2ee5c9063bdb05462359d12b537386
zyzo <zyzo@github.com>
2015-05-29 12:02:22 +0200

    #4 : global timezone support & IcsMerger::getRawText fix    * make use of global timezone from X-WR-TIMEZONE property    this global timezone will be used to convert datetime when the local timezone reference does not exists    * adapt getRawText to recent modification + add test case

--- ics-collector/debugger/tests/IcsMergerTest.php
+++ ics-collector/debugger/tests/IcsMergerTest.php
@@ -13,8 +13,8 @@ class IcsMergerTest extends PHPUnit_Framework_TestCase {
 	}
 
 	public function testWithTwoFile() {
-		$this->merger->add(file_get_contents('../data/freifunk_0'));
-		$this->merger->add(file_get_contents('../data/freifunk_3'));
+		$this->merger->add(file_get_contents(realpath(dirname(__FILE__)) . '/../data/freifunk_0'));
+		$this->merger->add(file_get_contents(realpath(dirname(__FILE__)) . '/../data/freifunk_3'));
 		$result = $this->merger->getResult();
 		$this->assertEquals(count($result['VEVENTS']), 3);
 		/*
@@ -49,6 +49,7 @@ class IcsMergerTest extends PHPUnit_Framework_TestCase {
 	public function testConversionLocalTimeWithoutTimezone() {
 		$this->merger->add
 		("BEGIN:VCALENDAR
+		  X-WR-TIMEZONE:Asia/Saigon
 		  BEGIN:VEVENT
 		  DTSTART:20150527T213000
 		  SUMMARY:Event 1
@@ -64,6 +65,15 @@ class IcsMergerTest extends PHPUnit_Framework_TestCase {
 		  END:VCALENDAR
 		");
 		$this->assertEquals($this->merger->getResult()['VEVENTS']['0']['DTSTART']['value'], '20150527T163000');
-		$this->assertEquals($this->merger->getResult()['VEVENTS']['1']['DTSTART']['value'], '20150528T023000');
+		$this->assertEquals($this->merger->getResult()['VEVENTS']['1']['DTSTART']['value'], '20150528T033000');
+	}
+
+	public function testGetRawText() {
+		$this->config = parse_ini_file(realpath(dirname(__FILE__)) . '/../lib/' . IcsMerger::CONFIG_FILENAME);
+		$defaultTimezone = $this->config['DEFAULT_TIMEZONE']; 
+		$text = "BEGIN:VCALENDAR\nX-WR-TIMEZONE:$defaultTimezone\nPRODID:-//FOSSASIA//FOSSASIA Calendar//EN\nBEGIN:VEVENT\nDTSTART:20150527T213000\nSUMMARY:Event 1\nEND:VEVENT\nEND:VCALENDAR";
+		$this->merger->add($text);
+		$result = $this->merger->getResult();
+		$this->assertEquals(IcsMerger::getRawText($result), $text, 'Result must be equal to initial input');
 	}
 }
