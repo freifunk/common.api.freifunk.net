58507b672a6220d6c18418729d06796b13708f74
zyzo <zyzo@github.com>
2015-06-03 00:47:05 +0200

    move ics-collector/updater.php -> ics-collector/lib/ics-updater.php

--- ics-collector/lib/ics-updater.php
+++ ics-collector/lib/ics-updater.php
@@ -1,6 +1,6 @@
 <?php
 
-require (realpath(dirname(__FILE__))  . '/lib/ics-merger.php');
+require (realpath(dirname(__FILE__))  . '/ics-merger.php');
 
 $configs = parse_ini_file('api-config.ini', true);
 $mergedIcsHeader = $configs['MERGED_ICS_HEADER'];
@@ -11,13 +11,13 @@ $merger = new IcsMerger($configs['MERGED_ICS_HEADER']);
 foreach($summary as $key => $value) {
 	echo 'Retrieving ics from ' . $key . '..' . PHP_EOL;
 	$ics = file_get_contents($value['url']);
-	$fp = fopen(realpath(dirname(__FILE__)) . '/data/' . $key . '.ics' , 'w+');
+	$fp = fopen(realpath(dirname(__FILE__)) . '/../data/' . $key . '.ics' , 'w+');
 	fwrite($fp, $ics);
 	fclose($fp);
 	$merger->add($ics, array($configs['CUSTOM_PROPERTY_NAME']['SOURCE_PROPERTY'] => $key));
 }
 
 echo 'Merge all ics files..' . PHP_EOL;
-$fp = fopen((realpath(dirname(__FILE__))  . '/data/ffMerged.ics'), 'w+');
+$fp = fopen((realpath(dirname(__FILE__))  . '/../data/ffMerged.ics'), 'w+');
 fwrite($fp, IcsMerger::getRawText($merger->getResult()));
 fclose($fp);
