60986016d2c323a0ca75fc90cbb0a5182b419274
zyzo <zyzo@github.com>
2015-06-03 00:42:23 +0200

    add api-config.ini file & updater.php script to retrieve all communities ics

---
+++ ics-collector/updater.php
@@ -0,0 +1,23 @@
+<?php
+
+require (realpath(dirname(__FILE__))  . '/lib/ics-merger.php');
+
+$configs = parse_ini_file('api-config.ini', true);
+$mergedIcsHeader = $configs['MERGED_ICS_HEADER'];
+
+$summary = file_get_contents('http://localhost/fossasia/common.api.freifunk.net/ics-collector/ics-collector.php?format=json');
+$summary = json_decode($summary, true);
+$merger = new IcsMerger($configs['MERGED_ICS_HEADER']);
+foreach($summary as $key => $value) {
+	echo 'Retrieving ics from ' . $key . '..' . PHP_EOL;
+	$ics = file_get_contents($value['url']);
+	$fp = fopen(realpath(dirname(__FILE__)) . '/data/' . $key . '.ics' , 'w+');
+	fwrite($fp, $ics);
+	fclose($fp);
+	$merger->add($ics, array($configs['CUSTOM_PROPERTY_NAME']['SOURCE_PROPERTY'] => $key));
+}
+
+echo 'Merge all ics files..' . PHP_EOL;
+$fp = fopen((realpath(dirname(__FILE__))  . '/data/ffMerged.ics'), 'w+');
+fwrite($fp, IcsMerger::getRawText($merger->getResult()));
+fclose($fp);
