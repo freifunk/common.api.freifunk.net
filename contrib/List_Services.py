ef758d7644e11155782f7795f623bdecbf24f565
Andrei Mitrofanow <smilebef@web.de>
2016-04-21 10:50:19 +0200

    geändert:       contrib/List_Services.py
    Die Datei List_Services wurde an die Summarized-URL:
    http://api.freifunk.net/data/ffSummarizedDir.json
    angepasst.

--- contrib/List_Services.py
+++ contrib/List_Services.py
@@ -5,37 +5,39 @@ import ujson, urllib
 from urlgrabber import urlopen
 from termcolor import colored
 
-URL="https://raw.githubusercontent.com/freifunk/directory.api.freifunk.net/master/directory.json"
-file="directory.json"
+URL="http://api.freifunk.net/data/ffSummarizedDir.json"
+file="ffSummarizedDir.json"
 
 try:
 	url=urlopen(URL)
-	Staat = ujson.loads(url.read())
+	Nation = ujson.loads(url.read())
 except:
 	print URL , "nicht gefunden - öffne locale Datei..."
-	Staat = ujson.loads(open(file,'r').read())
+	Nation = ujson.loads(open(file,'r').read())
 
-for Ort in Staat:
+
+for Ort in Nation:
 	if not "evernet" in Ort:
-		try:
-			Inhalt=urllib.urlopen(Staat[Ort])
-			JSON=ujson.loads(Inhalt.read().replace('\r',''))
-			Fehler=0
-		except:
-			print colored("Error in "+Ort+": ","red")
-			print Inhalt.read()
-			ujson.dumps(Inhalt.read())
-			print colored("Error while ujson.loads(Inhalt.read().replace('\r',''))","red")
-			Fehler=1
-		for Service in JSON:
+		Fehler=0
+		#try:
+		#	Inhalt=urllib.urlopen(Staat[Ort])
+		#	JSON=ujson.loads(Inhalt.read().replace('\r',''))
+		#	Fehler=0
+		#except:
+		#	print colored("Error in "+Ort+": ","red")
+		#	print Inhalt.read()
+		#	ujson.dumps(Inhalt.read())
+		#	print colored("Error while ujson.loads(Inhalt.read().replace('\r',''))","red")
+		#	Fehler=1
+		for Service in Nation[Ort]:
 			if "services" in Service:
 				print "____________________________________________________________________________________"
 				if Fehler:
-					print colored("Ort: "+str(Ort),"red") #,"\tLink",Staat[Ort]
+					print colored("Ort: "+str(Ort),"red") #,"\tLink",Nation[Ort]
 				else:
-					print "Ort: ",Ort#,"\tLink",Staat[Ort]
+					print "Ort: ",Ort#,"\tLink",Nation[Ort]
 				print "____________________________________________________________________________________"
-				for k in JSON[Service]:
+				for k in Nation[Ort][Service]:
 					print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 					for l in k:
 						print "-----------------------------------------------------------------"
